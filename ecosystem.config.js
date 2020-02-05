2da9c579455998e7c0edf4a7713f3f1848237554
Linus Norton <linusnorton@gmail.com>
2020-02-05 08:11:52 +0000

    step7

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps : [{
+        name: "Your app",
+        script: "npm start",
+        env: {
+            NODE_ENV: "development",
+        },
+        env_production: {
+            NODE_ENV: "production",
+        }
+    }],
+    deploy : {
+        production : {
+            "ssh_options": "StrictHostKeyChecking=no",
+            "key": "deploy",
+            "user": "ubuntu",
+            "host": ["3.8.141.48"],
+            "ref": "origin/master",
+            "repo": "git@github.com:linusnorton/continuous-deployment-demo.git",
+            "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production",
+            "env"  : {
+                "NODE_ENV": "production"
+            }
+        }
+    }
+};
